a116733a0ef346c281a3dcfaaae4cc8b42c9c05b
PengHK33 <hli0005@copperfieldcollege.vic.edu.au>
2025-06-25 12:52:40 +1000

    top frame

--- Gui.py
+++ Gui.py
@@ -9,4 +9,9 @@ app.grid_collumnconfigure(0, weight=1) # right column
 app.grid_rowconfigure(0, weight=1) # top bar
 app.grid_rowconfigure(1, weight=6) # main content
 app.grid_rowconfigure(2, weight=1) # footer
-
+#Top Frame
+topFrame = ctk.CTkFrame(app, border_width=4)
+topFrame.grid(row=0, collumn=0, columnspan=2, sticky="nsew",padx=5,pady=5)
+topFrame.grid_collumnconfigure(0,weight=1)
+topFrame.grid_collumnconfigure(1,weight=0)
+titleLabel = ctk.CTkLabel(topFrame, text ="Innovate Invoice",font=("Aptos",30))
